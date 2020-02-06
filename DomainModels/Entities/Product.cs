0f0815cae92ea79b308fdea8df87fdbf6a60cb68
Ajay Paritala <1aparitala@gmail.com>
2020-02-06 12:20:23 +0530

    Done

---
+++ DomainModels/Entities/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DomainModles
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        // [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "varchar(250)")]
+        [Required]
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        //reference naviation property
+        public Category Category { get; set; }
+    }
+}
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace EFCore
+//{
+//    class Product
+//    {
+
+//        public int ProductID { get; set; }
+//        public string ProductName { get; set; }
+//        public string Description { get; set; }
+//        public decimal UnitPrice { get; set; }
+//        public int CategoryID { get; set; }
+//    }
+//}
---
+++ WebApp/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+
+using EFCore;
+
+namespace WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        IUnitOfWork uow;
+
+        public ProductController()
+        {
+            uow = new UnitOfWork();
+        }
+        public IActionResult Index()
+        {
+
+            IEnumerable<Product> data = uow.ProductRepo.GetAll();
+            return View(data);
+        }
+    }
+}
